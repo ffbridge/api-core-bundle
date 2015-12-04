0eca99a6e675c109111a1317c95ec07bc43658d7
Charles Sanquer <csanquer@csanquer-kilix-xps13>
2015-12-04 17:38:36 +0100

    fix GITHUB_API_TOKEN env var in docker

--- dockerfiles/php/docker-entrypoint.sh
+++ dockerfiles/php/docker-entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [ $CHECK_SSH_GITLAB -gt 0 ];then
+if [ $CHECK_SSH_GITLAB -gt 0 ]; then
     ssh -o "StrictHostKeyChecking no" -q git@gitlab.kilix.net 2>&1 > /dev/null
 fi
 
-if [ -n $GITHUB_API_TOKEN ];then
+if [[ -n $GITHUB_API_TOKEN ]]; then
     composer config -g github-oauth.github.com $GITHUB_API_TOKEN
 fi
 
